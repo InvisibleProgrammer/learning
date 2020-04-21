ef172217d316658b101b2be3f8d5379573fc68a0
Zsolt Miskolczi <zsolt.miskolczi@gmail.com>
2020-04-21 11:47:11 +0200

    Learn some higher-order functions.

--- EloquentJavaScript/04 - Higher-Order Functions/01 - Flattening/index.js
+++ EloquentJavaScript/04 - Higher-Order Functions/01 - Flattening/index.js
@@ -1,11 +1,13 @@
-function flatten (...arrays) {
-    let flattenedArray = [];
 
-    flattenedArray = arrays.reduce((a, b) => a.concat(b));
-
-    return flattenedArray;
+const flatten = function (arrays) {
+    return arrays.reduce((array, concatenated) => array.concat(concatenated))
 }
+
+const test = [[1, 2, 3], [4, 5], [6]]
+
+console.log(flatten(test))
+
 
 const testArray = ['hello', 'world', 'of', 'arrays'];
 
-console.log(`flatten(testArray): ${flatten(testArray)} `);
+console.log(flatten(testArray));
