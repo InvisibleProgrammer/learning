46165e958c4247cc3b4d870ec3aa75cd07fabb49
Zsolt Miskolczi <zsolt.miskolczi@gmail.com>
2022-01-27 14:24:23 +0100

    Start GO course

---
+++ go/udemy-the-complete-developer-guide/cards/go.mod
@@ -0,0 +1,3 @@
+module invisibleprogrammer.com/cards
+
+go 1.17
---
+++ go/udemy-the-complete-developer-guide/cards/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
